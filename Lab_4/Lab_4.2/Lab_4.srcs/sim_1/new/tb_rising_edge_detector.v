5b2bd6eddbd4422142e03f1d0e8667bb6e009f78
Sam(Hyokwon) Chung <55061992+hyochung301@users.noreply.github.com>
2022-10-20 15:13:43 -0500

    4.2 initial

---
+++ Lab_4/Lab_4.2/Lab_4.srcs/sim_1/new/tb_rising_edge_detector.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/10/20 14:57:05
+// Design Name: 
+// Module Name: tb_rising_edge_detector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rising_edge_tb;
+
+reg clk;
+reg signal;
+reg reset;
+
+wire outedge;
+
+rising_edge ul (
+.clk(clk),
+.signal(signal),
+.reset(reset),
+.outedge(outedge)
+);
+
+initial begin
+    
+    clk = 0;
+    signal = 0;
+    reset = 0;
+    
+    #10;
+    
+    signal = 1;
+    reset = 0;
+    
+    #10;
+    
+    signal = 0;
+    reset = 1;
+    
+    #10;
+    
+    signal = 1;
+    reset = 1;
+    
+    #10;
+    
+    signal = 0;
+    reset = 0;
+    
+    #10;
+    
+    signal = 1;
+    reset = 0;
+    
+    #10
+    
+    reset = 1;
+    
+    #20;
+    
+    reset = 0;
+    
+    #20;
+    
+    signal = 0;
+    reset = 1;
+    
+    #20;
+    
+    signal = 0;
+    reset = 0;
+    
+end
+always
+    #5 clk = ~clk;
+
+endmodule
